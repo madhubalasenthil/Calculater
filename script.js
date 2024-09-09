264ef21a44680d27d256ebd4c7edf993919b8ca5
madhubalasenthil <160823415+madhubalasenthil@users.noreply.github.com>
2024-09-09 19:34:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = " ";
+}
+
+function calculate(){
+    display.value = eval(display.value);
+ 
+}
